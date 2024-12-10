bfd7408477dc851e540e6c003b2072e9f94e5aa8
ivan <ivacassim@alu.edu.gva.es>
2024-12-10 11:43:16 +0100

    Cacac

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
-require_once("config/sessionControl.php");
+require_once("config/sesionControl.php");
 require_once("router/router.php");
 require_once("views/layout/head.php");
 
